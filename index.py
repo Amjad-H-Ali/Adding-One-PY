4b25dc6f38fb850c971f8a2eac706b2aef400d9c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-16 20:17:37 -0500

    If last digit is nine, we set it equal to 0 and decrement i

--- index.py
+++ index.py
@@ -2,15 +2,21 @@ print ('Adding One')
 
 def adding_one(*array):
 
+	# Set i equal to last index in array by default
 	i = array[1] if len(array) > 1 else len(array[0]) - 1
 	
-
+	# Add one to last digit in array if it is less than nine
 	if array[0][i] < 9:
 		array[0][i] += 1
 
 		return array[0]
 
-	return i
+	array[0][i] = 0
+
+	i -= 1	
 
 
-print(adding_one([1, 3, 7, 2]))
+	return array[0], i
+
+
+print(adding_one([1, 3, 7, 9]))
